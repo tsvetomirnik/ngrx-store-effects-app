a0940df7f21792418f47073e383e2f00854085ef
Tsvetomir Nikolov <tsvetomir.nikolov@mentormate.com>
2019-02-25 13:00:26 +0200

    Added create pizza action

--- src/products/store/actions/pizzas.actions.ts
+++ src/products/store/actions/pizzas.actions.ts
@@ -1,6 +1,8 @@
 import { Action } from "@ngrx/store";
 import { Pizza } from "../../models/pizza.model";
 
+// Load Pizzas
+
 export const LOAD_PIZZAS = "[Products] Load Pizzas";
 export const LOAD_PIZZAS_FAIL = "[Products] Load Pizzas Fail";
 export const LOAD_PIZZAS_SUCCESS = "[Products] Load Pizzas Success";
@@ -19,4 +21,31 @@ export class LoadPizzasSuccess implements Action {
   constructor(public payload: Pizza[]) {}
 }
 
-export type PizzasAction = LoadPizzas | LoadPizzasFail | LoadPizzasSuccess;
+// Create pizza
+
+export const CREATE_PIZZA = "[Products] Create Pizza";
+export const CREATE_PIZZA_FAIL = "[Products] Create Pizza Fail";
+export const CREATE_PIZZA_SUCCESS = "[Products] Create Pizza Success";
+
+export class CreatePizza implements Action {
+  readonly type = CREATE_PIZZA;
+  constructor(public payload: Pizza) {}
+}
+
+export class CreatePizzaFail implements Action {
+  readonly type = CREATE_PIZZA_FAIL;
+  constructor(public payload: any) {}
+}
+
+export class CreatePizzaSuccess implements Action {
+  readonly type = CREATE_PIZZA_SUCCESS;
+  constructor(public payload: Pizza) {}
+}
+
+export type PizzasAction =
+  | LoadPizzas
+  | LoadPizzasFail
+  | LoadPizzasSuccess
+  | CreatePizza
+  | CreatePizzaFail
+  | CreatePizzaSuccess;
