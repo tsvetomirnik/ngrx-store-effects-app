487bf7fd87bf7151c4bd1f805eef781807b9c453
Tsvetomir Nikolov <tsvetomir.nikolov@mentormate.com>
2019-03-08 15:26:06 +0200

    Added pizza guard

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -13,6 +13,9 @@ import * as fromComponents from "./components";
 // containers
 import * as fromContainers from "./containers";
 
+// guards
+import * as fromGuards from "./guards";
+
 // services
 import * as fromServices from "./services";
 
@@ -20,10 +23,12 @@ import * as fromServices from "./services";
 export const ROUTES: Routes = [
   {
     path: "",
+    canActivate: [fromGuards.PizzasGuard],
     component: fromContainers.ProductsComponent
   },
   {
     path: "new",
+    canActivate: [fromGuards.PizzasGuard],
     component: fromContainers.ProductItemComponent
   },
   {
@@ -41,7 +46,7 @@ export const ROUTES: Routes = [
     StoreModule.forFeature("products", reducers),
     EffectsModule.forFeature(effects)
   ],
-  providers: [...fromServices.services],
+  providers: [...fromServices.services, ...fromGuards.guards],
   declarations: [...fromContainers.containers, ...fromComponents.components],
   exports: [...fromContainers.containers, ...fromComponents.components]
 })
